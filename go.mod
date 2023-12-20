5df89b1e0f8ca931942417149bf86d2b5ea2ce35
Naveen Kumar Reddy Molakathalla <naveenreddy.m@in.ibm.com>
2023-12-20 21:44:12 +0530

    fifth

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NaveenMolakathalla/pnaveen
 
 go 1.21.5
+
+require github.com/NaveenMolakathalla/dnaveen v0.0.0-20231220155944-8248fc9fc0e7 // indirect
--- pnaveen.go
+++ pnaveen.go
@@ -1,9 +1,21 @@
 package pnaveen
 
+import (
+	"github.com/NaveenMolakathalla/dnaveen"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dnaveen.WhenGrownUp(Bark())
+}
+
+func BigBarsk() string {
+	return dnaveen.WhenGrownUp(Barks())
 }
